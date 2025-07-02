94552d5a778f77e9dc39709a8803b2ba2aed02ea
NguyenVanTri <125877986+Tri3619@users.noreply.github.com>
2025-07-02 12:28:56 +0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,4 +1,3 @@
-// Register File
 module RegisterFile(
     input         clk,
     input         we,
@@ -10,8 +9,14 @@ module RegisterFile(
     output [31:0] rd2
 );
     reg [31:0] registers [0:31];
+    
+    // Khởi tạo tất cả thanh ghi = 0
+    integer i;
+    initial begin
+        for (i = 0; i < 32; i = i + 1)
+            registers[i] = 32'b0;
+    end
 
-    // Register x0 is hardwired to zero
     assign rd1 = (a1 != 0) ? registers[a1] : 0;
     assign rd2 = (a2 != 0) ? registers[a2] : 0;
 
