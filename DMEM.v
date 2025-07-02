e214ff7d115c04ac472f7052a03084b22715a40d
NguyenVanTri <125877986+Tri3619@users.noreply.github.com>
2025-07-02 12:33:35 +0700

    Update DMEM.v

--- DMEM.v
+++ DMEM.v
@@ -3,9 +3,10 @@ module DMEM(
     input         we,
     input  [31:0] addr,
     input  [31:0] wd,
-    output [31:0] rd,
-    output reg [31:0] memory [0:1023]  // Thêm output này
+    output [31:0] rd
 );
+    reg [31:0] memory [0:1023];  // Khai báo bộ nhớ
+
     initial begin
         $readmemh("./mem/dmem_init.hex", memory);
     end
